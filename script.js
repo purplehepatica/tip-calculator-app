2c1cf5460d33b2cba8d2079f0a95ba24b7f3fff8
Jakub <102424460+purplehepatica@users.noreply.github.com>
2023-06-06 19:30:18 +0200

    Update

--- script.js
+++ script.js
@@ -20,30 +20,93 @@ const numberOfPeople = () => {
 
 
 
+
+
+
+const tipPercentages = document.querySelector(".tip-percentage").querySelectorAll(".percent");
+
+
+
 document.querySelector(".five").addEventListener("click", function() {
+
+  const allPercentItems = document.querySelectorAll(".percent");
+  for (let thisPercent of allPercentItems) {
+    thisPercent.classList.remove("active");
+  }
+  document.querySelector(".five").classList.add("active");
+
   tipAmount(5);
   totalAmount(5);
 })
 
 document.querySelector(".ten").addEventListener("click", function() {
+
+  const allPercentItems = document.querySelectorAll(".percent");
+  for (let thisPercent of allPercentItems) {
+    thisPercent.classList.remove("active");
+  }
+
+  document.querySelector(".ten").classList.add("active");
+
   tipAmount(10);
   totalAmount(10);
 })
 
 document.querySelector(".fifteen").addEventListener("click", function() {
+
+    const allPercentItems = document.querySelectorAll(".percent");
+    for (let thisPercent of allPercentItems) {
+      thisPercent.classList.remove("active");
+    }
+
+    document.querySelector(".fifteen").classList.add("active");
+
   tipAmount(15);
   totalAmount(15);
 })
 
 document.querySelector(".twentyfive").addEventListener("click", function() {
+
+  const allPercentItems = document.querySelectorAll(".percent");
+  for (let thisPercent of allPercentItems) {
+    thisPercent.classList.remove("active");
+  }
+
+  document.querySelector(".twentyfive").classList.add("active");
+
   tipAmount(25);
   totalAmount(25);
 })
 
 document.querySelector(".fifty").addEventListener("click", function() {
+
+  const allPercentItems = document.querySelectorAll(".percent");
+  for (let thisPercent of allPercentItems) {
+    thisPercent.classList.remove("active");
+  }
+
+  document.querySelector(".fifty").classList.add("active");
+
   tipAmount(50);
   totalAmount(50);
 })
+
+document.querySelector(".custom").addEventListener("click", function() {
+
+  const allPercentItems = document.querySelectorAll(".percent");
+  for (let thisPercent of allPercentItems) {
+    thisPercent.classList.remove("active");
+  }
+
+  document.querySelector(".custom").classList.add("active");
+
+  const customTipValue = document.querySelector(".custom").value;
+  tipAmount(customTipValue);
+  totalAmount(customTipValue);
+})
+
+
+
 
 
 
@@ -56,6 +119,11 @@ function tipAmount(tipValue) {
 
   if (numberOfPeople() === null || numberOfPeople() === 0) {
     document.querySelector(".tip-amount").innerText = "$0.00";
+    return "$0.00"
+  };
+
+  if (tipValue === null || tipValue < 0) {
+    document.querySelector(".total").innerText = "$0.00";
     return "$0.00"
   };
 
@@ -74,6 +142,11 @@ function totalAmount(tipValue) {
   };
 
   if (numberOfPeople() === null || numberOfPeople() === 0) {
+    document.querySelector(".total").innerText = "$0.00";
+    return "$0.00"
+  };
+
+  if (tipValue === null || tipValue < 0) {
     document.querySelector(".total").innerText = "$0.00";
     return "$0.00"
   };
